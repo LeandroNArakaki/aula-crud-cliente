f231b6f228144081c8643ba1f0c8900c056e8908
Leandro Arakaki <leanarakaki@gmail.com>
2023-12-09 20:27:17 -0400

    primeiro commit

---
+++ src/main/java/com/devsuperior/aulacrudclient/AulaCrudClientApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.aulacrudclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaCrudClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaCrudClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/aulacrudclient/AulaCrudClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.aulacrudclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaCrudClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
